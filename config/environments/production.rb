eb65880b3d6a774126c8408eb88eccb4f414c8f3
katherine <muedasnka@gmail.com>
2015-04-18 21:33:50 -0600

    fixs email problems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,13 +73,14 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV['DOMAIN_NAME'],
+    port: "587",
+    domain: 'gmail.com',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD']
   }
+
   # ActionMailer Config
   config.action_mailer.default_url_options = { host: ENV['DOMAIN_NAME'] }
   config.action_mailer.delivery_method = :smtp
