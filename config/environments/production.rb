ef7465eabb3284f7734d175c6ceaa72fee16561b
Quintin Adam <quintinadam@live.com>
2015-04-19 11:22:59 -0600

    raise errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV['DOMAIN_NAME'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
