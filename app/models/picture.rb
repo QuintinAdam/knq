fe1b69051003a6aa97e427ae504ee70df618027b
katherine <muedasnka@gmail.com>
2015-06-21 15:11:56 -0600

    adds categories to enum

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ActiveRecord::Base
-  enum category: [:memories, :engagement] # , :wedding, :the_ceremony, :our_reception, :the_dress, :bouquet, :honeymoon
+  enum category: [:memories, :engagement, :rehearsal_dinner, :wedding] # , :wedding, :the_ceremony, :our_reception, :the_dress, :bouquet, :honeymoon
   belongs_to :user
   has_attached_file :photo, styles: { large: "600x600>", large_square: "500X500#", medium: "300x300>", medium_square: "200x200#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
